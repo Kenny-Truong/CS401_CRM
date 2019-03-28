ec1356d432b4a13501ec8bc6963fa4320559f615
Johannes <johannesmendoza327@gmail.com>
2019-03-28 13:28:56 -0700

    Histogram for Analytics

---
+++ CRM/src/Analytics/model/Histogram.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analytics.model;
+
+import java.util.Map;
+
+/**
+ *
+ * @author Jomar
+ */
+public class Histogram {
+    
+    int numberRows;
+    Map data;
+    public Histogram(int numRows, Map _data){
+        numberRows = numRows;
+        data = _data;        
+    }
+            
+    
+    
+    
+}
